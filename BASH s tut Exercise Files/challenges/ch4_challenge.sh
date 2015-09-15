b8cc4e974257d5c0cb919b8038a5a3ddfc6377ba
Erfder <caherman@fullsail.edu>
2015-09-14 22:57:52 -0600

    Challenge 2 done

--- BASH s tut Exercise Files/challenges/ch4_challenge.sh
+++ BASH s tut Exercise Files/challenges/ch4_challenge.sh
@@ -4,3 +4,15 @@
 # Write a simple guessing game, using interactive input and a test condition. 
 #+Also, build the program so that it responds to a command line argument
 #+and also has a function if no argument is specified. Use a function as well.
+function stuff {
+	read -p "Guess what my favorite color is " a
+	if [[ -z $a ]]; then
+		echo "you did not enter an answer."
+	elif [[ $a != blue ]]; then
+		echo "$a is wrong, sorry. To escape, leave it blank."
+		stuff	
+	else
+		echo "correct! $a is my favorite color!"
+	fi
+}
+stuff
